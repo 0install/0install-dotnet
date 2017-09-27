9c4fae73083a6605de518c56c8402d7b5c7889a7
Bastian Eicher <bastian@eicher.net>
2017-09-27 17:49:57 +0200

    Added RefreshingSolver

---
+++ src/Services/Solvers/RefreshingSolver.cs
@@ -0,0 +1,69 @@
+﻿/*
+ * Copyright 2010-2017 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Net;
+using JetBrains.Annotations;
+using NanoByte.Common;
+using ZeroInstall.Services.Feeds;
+using ZeroInstall.Store.Model;
+using ZeroInstall.Store.Model.Selection;
+
+namespace ZeroInstall.Services.Solvers
+{
+    /// <summary>
+    /// A wrapper around an <see cref="ISolver"/> that handles automatic refreshing of stale feeds.
+    /// </summary>
+    /// <remarks>Do not use this if you want manual control over when refreshing happens, e.g. to ensure fast startup time of cached implementations.</remarks>
+    public class RefreshingSolver : ISolver
+    {
+        #region Dependencies
+        private readonly ISolver _innerSolver;
+        private readonly IFeedManager _feedManager;
+
+        public RefreshingSolver([NotNull] ISolver innerSolver, [NotNull] IFeedManager feedManager)
+        {
+            _innerSolver = innerSolver ?? throw new ArgumentNullException(nameof(innerSolver));
+            _feedManager = feedManager ?? throw new ArgumentNullException(nameof(feedManager));
+        }
+        #endregion
+
+        /// <inheritdoc/>
+        public Selections Solve(Requirements requirements)
+        {
+            var selections = _innerSolver.Solve(requirements);
+
+            if (_feedManager.ShouldRefresh)
+            {
+                Log.Info("Running Refresh Solve because feeds have become stale");
+
+                _feedManager.Stale = false;
+                _feedManager.Refresh = true;
+                try
+                {
+                    selections = _innerSolver.Solve(requirements);
+                }
+                catch (WebException ex)
+                {
+                    Log.Warn(ex.Message);
+                }
+            }
+
+            return selections;
+        }
+    }
+}
---
+++ src/UnitTests/Services/Solvers/RefreshingSolverTest.cs
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright 2010-2017 Bastian Eicher
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using FluentAssertions;
+using Moq;
+using Xunit;
+using ZeroInstall.Services.Feeds;
+using ZeroInstall.Store.Model;
+using ZeroInstall.Store.Model.Selection;
+
+namespace ZeroInstall.Services.Solvers
+{
+    public class RefreshingSolverTest : TestWithMocks
+    {
+        private readonly Requirements _requirements = RequirementsTest.CreateTestRequirements();
+        private readonly Selections _selections = SelectionsTest.CreateTestSelections();
+
+        private readonly Mock<IFeedManager> _feedManagerMock;
+        private readonly Mock<ISolver> _innerSolverMock;
+        private readonly ISolver _solver;
+
+        public RefreshingSolverTest()
+        {
+            _innerSolverMock = CreateMock<ISolver>();
+            _innerSolverMock.Setup(x => x.Solve(_requirements)).Returns(_selections);
+
+            _feedManagerMock = CreateMock<IFeedManager>();
+
+            _solver = new RefreshingSolver(_innerSolverMock.Object, _feedManagerMock.Object);
+        }
+
+        [Fact]
+        public void ShouldNotRefreshFreshFeeds()
+        {
+            _feedManagerMock.SetupGet(x => x.ShouldRefresh).Returns(false);
+
+            _solver.Solve(_requirements).Should().Be(_selections);
+
+            _innerSolverMock.Verify(x => x.Solve(_requirements), Times.Exactly(1));
+        }
+
+        [Fact]
+        public void ShouldRefreshStaleFeeds()
+        {
+            _feedManagerMock.SetupGet(x => x.ShouldRefresh).Returns(true);
+            _feedManagerMock.SetupSet(x => x.Refresh = true).Verifiable();
+            _feedManagerMock.SetupSet(x => x.Stale = false).Verifiable();
+
+            _solver.Solve(_requirements).Should().Be(_selections);
+
+            _innerSolverMock.Verify(x => x.Solve(_requirements), Times.Exactly(2));
+        }
+    }
+}
