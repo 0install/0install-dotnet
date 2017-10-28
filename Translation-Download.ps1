16a5ad7543e8d97a2b90a4e8f4b7e9a8f87a71fd
Bastian Eicher <bastian@eicher.net>
2017-10-28 17:20:40 +0200

    Added scripts for downloading/uploading translations

---
+++ Translation-Download.ps1
@@ -0,0 +1,16 @@
+Param ([Parameter(Mandatory=$True)][String]$User, [Parameter(Mandatory=$True)][String]$Password)
+$ErrorActionPreference = "Stop"
+$ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+function get ($relativeUri, $filePath) {
+    0install run http://repo.roscidus.com/utils/curl -k -L --user "${User}:${Password}" -o $filePath https://www.transifex.com/api/2/project/0install-win/$relativeUri
+}
+
+function download($slug, $pathBase) {
+    get "resource/$slug/translation/el/?file" "$pathBase.el.resx"
+    get "resource/$slug/translation/tr/?file" "$pathBase.tr.resx"
+}
+
+download store "$ScriptDir\src\Store\Properties\Resources"
+download services "$ScriptDir\src\Services\Properties\Resources"
+download desktopintegration "$ScriptDir\src\DesktopIntegration\Properties\Resources"
---
+++ Translation-Upload.ps1
@@ -0,0 +1,16 @@
+﻿Param ([Parameter(Mandatory=$True)][String]$User, [Parameter(Mandatory=$True)][String]$Password)
+$ErrorActionPreference = "Stop"
+$ScriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+
+function put ($relativeUri, $filePath) {
+    0install run http://repo.roscidus.com/utils/curl -k -L --user "${User}:${Password}" -i -X PUT -F "file=@$filePath" "https://www.transifex.com/api/2/project/0install-win/$relativeUri"
+}
+
+function upload($slug, $pathBase) {
+    put "resource/$slug/content/" "$pathBase.resx"
+    put "resource/$slug/translation/de/" "$pathBase.de.resx"
+}
+
+upload store "$ScriptDir\src\Store\Properties\Resources"
+upload services "$ScriptDir\src\Services\Properties\Resources"
+upload desktopintegration "$ScriptDir\src\DesktopIntegration\Properties\Resources"
