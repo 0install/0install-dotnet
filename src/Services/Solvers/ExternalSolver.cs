e5e06762c8ba8f0f2c6c22ae531f0088dfb1fce7
Bastian Eicher <bastian@eicher.net>
2017-11-30 02:12:13 +0100

    ExternalSolver: Hacky workaround for sporadic race conditions (files created by external process not immediately visible to parent)

--- src/Services/Solvers/ExternalSolver.cs
+++ src/Services/Solvers/ExternalSolver.cs
@@ -21,6 +21,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using JetBrains.Annotations;
 using NanoByte.Common;
 using NanoByte.Common.Collections;
@@ -112,6 +113,9 @@ namespace ZeroInstall.Services.Solvers
                     }
                     control.HandleStderr();
                 }
+
+                // Hacky workaround for sporadic race conditions (files created by external process not immediately visible to parent)
+                Thread.Sleep(1000);
             }));
 
             return selections;
