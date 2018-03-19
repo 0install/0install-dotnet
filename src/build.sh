1dcbd909e5514f391d5b1c7a9349892c6740b0dd
Bastian Eicher <bastian@eicher.net>
2018-03-19 18:39:32 +0100

    Simplified building on non-Windows platforms

--- src/build.sh
+++ src/build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 
 echo "WARNING: You need Visual Studio on Windows to perform a full Release build"
 if hash msbuild 2>/dev/null; then
-    msbuild -t:Restore -t:Build -p:Version=${1:-1.0-dev}
+    msbuild -v:Quiet -t:Restore -t:Build -p:Version=${1:-1.0-dev}
 else
-    dotnet build --configuration DebugLinuxCore -p:Version=${1:-1.0-dev}
+    dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=DebugLinuxCore -p:Version=${1:-1.0-dev}
 fi
