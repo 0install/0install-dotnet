d26ade4ab7d505adbf6cd42ac2591f71a8d55b79
Bastian Eicher <bastian@eicher.net>
2018-03-21 00:03:32 +0100

    Tweaked build scripts

--- doc/build.ps1
+++ doc/build.ps1
@@ -1,15 +1,19 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# Ensure 0install is in the PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-	mkdir -Force "$env:TEMP\zero-install" | Out-Null
-	Invoke-WebRequest "https://0install.de/files/zero-install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
-	$env:PATH = "$env:TEMP\zero-install;$env:PATH"
+    # Put 0install in PATH using Bootstrapper
+    mkdir -Force "$env:TEMP\zero-install" | Out-Null
+    Invoke-WebRequest "https://0install.de/files/zero-install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
+    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
 }
 
 if (Test-Path ..\build\Documentation) {rm -Recurse -Force ..\build\Documentation}
 mkdir ..\build\Documentation | Out-Null
+
+# Download tag files for external references
+Invoke-WebRequest http://nano-byte.de/common/api/nanobyte-common.tag -OutFile nanobyte-common.tag
+
 0install run --batch http://0install.de/feeds/Doxygen.xml
 
 popd
--- doc/build.sh
+++ doc/build.sh
@@ -4,4 +4,8 @@ cd `dirname $0`
 
 rm -rf ../build/Documentation
 mkdir -p ../build/Documentation
+
+# Download tag files for external references
+curl -o nanobyte-common.tag http://nano-byte.de/common/api/nanobyte-common.tag
+
 0install run http://0install.de/feeds/Doxygen.xml
