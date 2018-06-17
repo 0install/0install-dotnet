c13d0700e83da94e4290676f62653c5464488560
Bastian Eicher <bastian@eicher.net>
2018-06-17 21:14:16 +0200

    Fixed publishing of .NET Core build of "0install"

--- src/build.ps1
+++ src/build.ps1
@@ -10,6 +10,7 @@ if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswher
   $msBuild = "dotnet msbuild"
 }
 . $msBuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=$Version
+. $msBuild -v:Quiet -t:Publish -p:NoBuild=True -p:BuildProjectReferences=False -p:Configuration=Release -p:TargetFramework=netcoreapp2.0 -p:Version=$Version Commands
 
 # Create snapshot of XML Schemas
 if (!(Test-Path ..\build\Schemas)) { mkdir ..\build\Schemas | Out-Null }
