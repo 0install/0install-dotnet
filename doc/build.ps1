d901dd0e400d3520dace816989d851394ae528c5
Bastian Eicher <bastian@eicher.net>
2018-07-28 01:00:51 +0200

    Misc build tweaks

--- doc/build.ps1
+++ doc/build.ps1
@@ -2,11 +2,7 @@
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 # Ensure 0install is in the PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
-    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
-}
+if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Resolve-Path ..\artifacts\Release\net45);$env:PATH" }
 
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
--- release/build.ps1
+++ release/build.ps1
@@ -3,11 +3,7 @@ $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 # Ensure 0install is in the PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
-    mkdir -Force "$env:TEMP\zero-install" | Out-Null
-    Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
-    $env:PATH = "$env:TEMP\zero-install;$env:PATH"
-}
+if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Resolve-Path ..\artifacts\Release\net45);$env:PATH" }
 
 # Build feed and archive
 $stability = if($Version.Contains('-')) {'developer'} else {'stable'}
