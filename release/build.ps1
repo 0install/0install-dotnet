377836ec248d244e2ee0054042e4675f8101f8f3
Bastian Eicher <bastian@eicher.net>
2018-07-28 10:52:00 +0200

    Removed logic for patching feeds to point to GitHub Releases

--- release/build.ps1
+++ release/build.ps1
@@ -1,4 +1,4 @@
-﻿Param ($Version = "0.1.0-pre", [switch]$GitHubRelease)
+﻿Param ($Version = "0.1.0-pre")
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
@@ -9,13 +9,5 @@ if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Reso
 $stability = if($Version.Contains('-')) {'developer'} else {'stable'}
 cmd /c "0install run --batch http://0install.net/tools/0template.xml 0install-dotnet.xml.template version=$Version stability=$stability 2>&1" # Redirect stderr to stdout
 if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
-
-# Patch archive URL to point to GitHub Release
-if ($GitHubRelease) {
-    $path = Resolve-Path "0install-dotnet-$Version.xml"
-    [xml]$xml = Get-Content $path
-    $xml.interface.group.implementation.archive.href = "https://github.com/0install/0install-dotnet/releases/download/$Version/$($xml.interface.group.implementation.archive.href)"
-    $xml.Save($path)
-}
 
 popd
