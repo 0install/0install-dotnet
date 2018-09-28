f3fc6357a5572461fece258fd5d11ab1f1685e79
Bastian Eicher <bastian@eicher.net>
2018-09-28 16:56:09 +0200

    Misc tweaks

--- doc/build.ps1
+++ doc/build.ps1
@@ -2,7 +2,9 @@
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
 # Ensure 0install is in the PATH
-if (!(Get-Command 0install -ErrorAction SilentlyContinue)) { $env:PATH = "$(Resolve-Path ..\artifacts\Release\net45);$env:PATH" }
+if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+    $env:PATH = "$(Resolve-Path ..\artifacts\Release\net45);$env:PATH"
+}
 
 if (Test-Path ..\artifacts\Documentation) {rm -Recurse -Force ..\artifacts\Documentation}
 mkdir ..\artifacts\Documentation | Out-Null
