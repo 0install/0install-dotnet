8a32a4779553faf57312215ca802c4b20d083610
Bastian Eicher <bastian@eicher.net>
2019-05-06 10:16:01 +0200

    Added support for VS2019 to build script

--- src/build.ps1
+++ src/build.ps1
@@ -4,7 +4,7 @@ pushd $PSScriptRoot
 
 if (Test-Path "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe") {
   $vsDir = . "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -latest -property installationPath -format value
-  $msBuild = "$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"
+  $msBuild = if (Test-Path "$vsDir\MSBuild\Current") {"$vsDir\MSBuild\Current\Bin\amd64\MSBuild.exe"} else {"$vsDir\MSBuild\15.0\Bin\amd64\MSBuild.exe"}
 } else {
   Write-Host -ForegroundColor yellow "WARNING: You need Visual Studio to perform a full Release build"
   $msBuild = "dotnet msbuild"
