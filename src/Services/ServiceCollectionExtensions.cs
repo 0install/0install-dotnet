ff008373fd88a70b1b45f448ea419db7e44765c5
Bastian Eicher <bastian@eicher.net>
2020-03-08 22:09:24 +0100

    Removed RefreshingSolver

--- src/Services/ServiceCollectionExtensions.cs
+++ src/Services/ServiceCollectionExtensions.cs
@@ -30,49 +30,32 @@ namespace ZeroInstall.Services
         /// </summary>
         /// <typeparam name="TTaskHandler">A callback object used when the the user needs to be asked questions or informed about download and IO tasks.</typeparam>
         /// <param name="services">The service collection to add the services to.</param>
-        /// <param name="autoRefresh"><c>true</c> to wrap the default <see cref="ISolver"/> in a <see cref="RefreshingSolver"/>.</param>
-        public static IServiceCollection AddZeroInstall<TTaskHandler>(this IServiceCollection services, bool autoRefresh = false) where TTaskHandler : class, ITaskHandler
-        {
-            #region Sanity checks
-            if (services == null) throw new ArgumentNullException(nameof(services));
-            #endregion
-
-            services
-                .AddScoped<ITaskHandler, TTaskHandler>()
-                .AddScoped(x => Config.Load())
-                .AddScoped(x => ImplementationStores.Default())
-                .AddScoped(x => OpenPgp.Verifying())
-                .AddScoped(x => FeedCaches.Default(x.GetService<IOpenPgp>()))
-                .AddScoped(x => TrustDB.LoadSafe())
-                .AddScoped<ITrustManager, TrustManager>()
-                .AddScoped<IFeedManager, FeedManager>()
-                .AddScoped<ICatalogManager, CatalogManager>()
-                .AddScoped(x => PackageManagers.Default())
-                .AddScoped<ISelectionsManager, SelectionsManager>()
-                .AddScoped<IFetcher, SequentialFetcher>()
-                .AddScoped<IExecutor, Executor>()
-                .AddScoped<ISelectionCandidateProvider, SelectionCandidateProvider>();
-
-            if (autoRefresh)
-            {
-                services
-                    .AddScoped<BacktrackingSolver>()
-                    .AddScoped<ISolver>(x => new RefreshingSolver(x.GetService<BacktrackingSolver>(), x.GetService<IFeedManager>()));
-            }
-            else services.AddScoped<ISolver, BacktrackingSolver>();
-
-            return services;
-        }
+        public static IServiceCollection AddZeroInstall<TTaskHandler>(this IServiceCollection services)
+            where TTaskHandler : class, ITaskHandler
+            => services.AddScoped<ITaskHandler, TTaskHandler>()
+                       .AddScoped(x => Config.Load())
+                       .AddScoped(x => ImplementationStores.Default())
+                       .AddScoped(x => OpenPgp.Verifying())
+                       .AddScoped(x => FeedCaches.Default(x.GetService<IOpenPgp>()))
+                       .AddScoped(x => TrustDB.LoadSafe())
+                       .AddScoped<ITrustManager, TrustManager>()
+                       .AddScoped<IFeedManager, FeedManager>()
+                       .AddScoped<ICatalogManager, CatalogManager>()
+                       .AddScoped(x => PackageManagers.Default())
+                       .AddScoped<ISelectionsManager, SelectionsManager>()
+                       .AddScoped<ISolver, BacktrackingSolver>()
+                       .AddScoped<IFetcher, SequentialFetcher>()
+                       .AddScoped<IExecutor, Executor>()
+                       .AddScoped<ISelectionCandidateProvider, SelectionCandidateProvider>();
 
         /// <summary>
         /// Registers a set of scoped services for using Zero Install functionality.
         /// Automatically uses <see cref="ILogger{TCategoryName}"/> and <see cref="ICredentialProvider"/> if registered in <paramref name="services"/>.
         /// </summary>
         /// <param name="services">The service collection to add the services to.</param>
-        /// <param name="autoRefresh"><c>true</c> to wrap the default <see cref="ISolver"/> in a <see cref="RefreshingSolver"/>.</param>
         /// <seealso cref="ConfigurationCredentialProviderRegisration.ConfigureCredentials"/>
-        public static IServiceCollection AddZeroInstall(this IServiceCollection services, bool autoRefresh = false)
-            => services.AddZeroInstall<ServiceTaskHandler>(autoRefresh);
+        public static IServiceCollection AddZeroInstall(this IServiceCollection services)
+            => services.AddZeroInstall<ServiceTaskHandler>();
     }
 }
 #endif
