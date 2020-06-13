a937a24718186561b420467c7ab39db1cd1a3542
Bastian Eicher <bastian@eicher.net>
2020-06-13 14:49:32 +0200

    Added unit test for ServiceCollection.AddZeroInstall()

---
+++ src/UnitTests/Services/ServiceCollectionTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright Bastian Eicher et al.
+// Licensed under the GNU Lesser Public License
+
+#if NETCOREAPP
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+using ZeroInstall.Services.Executors;
+using ZeroInstall.Services.Fetchers;
+using ZeroInstall.Services.Solvers;
+
+namespace ZeroInstall.Services
+{
+    public class ServiceCollectionTest
+    {
+        [Fact]
+        public void TestDependencyResolution()
+        {
+            var provider = new ServiceCollection().AddZeroInstall().BuildServiceProvider();
+            provider.GetRequiredService<ISolver>();
+            provider.GetRequiredService<IFetcher>();
+            provider.GetRequiredService<IExecutor>();
+        }
+    }
+}
+#endif
