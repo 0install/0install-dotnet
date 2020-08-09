da638e153352de1fdb6ec5d5aa79bad232f9872d
Bastian Eicher <bastian@eicher.net>
2020-08-09 11:43:35 +0200

    Added support for solid RAR archives

--- src/Store/Implementations/Archives/RarExtractor.cs
+++ src/Store/Implementations/Archives/RarExtractor.cs
@@ -2,16 +2,21 @@
 // Licensed under the GNU Lesser Public License
 
 using System.IO;
-using SharpCompress.Archives.Rar;
+using NanoByte.Common.Tasks;
+using SharpCompress.Common;
 using SharpCompress.Readers;
+using SharpCompress.Readers.Rar;
+using ZeroInstall.Store.Properties;
 
 namespace ZeroInstall.Store.Implementations.Archives
 {
     /// <summary>
     /// Extracts a RAR archive.
     /// </summary>
-    public class RarExtractor : SharpCompressArchiveExtractor
+    public class RarExtractor : ArchiveExtractor
     {
+        private readonly RarReader _reader;
+
         /// <summary>
         /// Prepares to extract a RAR archive contained in a stream.
         /// </summary>
@@ -19,7 +24,51 @@ namespace ZeroInstall.Store.Implementations.Archives
         /// <param name="targetPath">The path to the directory to extract into.</param>
         /// <exception cref="IOException">The archive is damaged.</exception>
         internal RarExtractor(Stream stream, string targetPath)
-            : base(RarArchive.Open(stream, new ReaderOptions {LeaveStreamOpen = false}), targetPath)
-        {}
+            : base(targetPath)
+        {
+            _reader = RarReader.Open(stream, new ReaderOptions {LeaveStreamOpen = false});
+            UnitsTotal = stream.Length;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing) _reader.Dispose();
+        }
+
+        /// <inheritdoc/>
+        protected override void ExtractArchive()
+        {
+            State = TaskState.Data;
+
+            try
+            {
+                while (_reader.MoveToNextEntry())
+                {
+                    var entry = _reader.Entry;
+
+                    string? relativePath = GetRelativePath(entry.Key.Replace('\\', '/'));
+                    if (relativePath == null) continue;
+
+                    if (entry.IsDirectory) DirectoryBuilder.CreateDirectory(relativePath, entry.LastModifiedTime?.ToUniversalTime());
+                    else
+                    {
+                        CancellationToken.ThrowIfCancellationRequested();
+
+                        string absolutePath = DirectoryBuilder.NewFilePath(relativePath, entry.LastModifiedTime?.ToUniversalTime());
+                        using (var fileStream = File.Create(absolutePath))
+                            _reader.WriteEntryTo(fileStream);
+
+                        UnitsProcessed += entry.CompressedSize;
+                    }
+                }
+            }
+            #region Error handling
+            catch (ExtractionException ex)
+            {
+                // Wrap exception since only certain exception types are allowed
+                throw new IOException(Resources.ArchiveInvalid + "\n" + ex.Message, ex);
+            }
+            #endregion
+        }
     }
 }
