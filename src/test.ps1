c588fa639276ce23c1f4d7372156aef5ef83ce05
Bastian Eicher <bastian@eicher.net>
2020-10-07 18:10:11 +0200

    Fixed test scripts

--- src/test.ps1
+++ src/test.ps1
@@ -5,7 +5,7 @@ function Run-DotNet {
     if (Get-Command dotnet -ErrorAction SilentlyContinue) {
         dotnet @args
     } else {
-        ..\_0install.ps1 run --batch --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml @args
+        ..\0install.ps1 run --batch --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml @args
     }
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
--- src/test.sh
+++ src/test.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 if command -v dotnet > /dev/null 2> /dev/null; then
     dotnet="dotnet"
 else
-    dotnet="../_0install.sh run --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml"
+    dotnet="../0install.sh run --version 3.1..!3.2 https://apps.0install.net/dotnet/core-sdk.xml"
 fi
 
 # Unit tests (without .NET Framework)
