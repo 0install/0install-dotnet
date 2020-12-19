f8dc713dcce03b2f6b6685d8f9447bb6a7a4f757
Bastian Eicher <bastian@eicher.net>
2020-12-19 20:44:02 +0100

    Tweaked shell scripts

--- src/Commands/Basic/Exporters/import.sh
+++ src/Commands/Basic/Exporters/import.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+shopt -s nullglob
 
-CONTENT_DIR=`dirname $0`/content
-for f in "$CONTENT_DIR/*.xml"; do 0install import "$f"; done
-for f in "$CONTENT_DIR/*.tbz2"; do 0install store add $(basename "$f" .tbz2) "$f"; done
+cd $(dirname $0)/content
+for f in *.xml; do 0install import "$f"; done
+for f in *.tbz2; do 0install store add "$(basename "$f" .tbz2)" "$f"; done
