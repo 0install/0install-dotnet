dd2d3176dece21ef213f64d0c0a62530679d596d
Bastian Eicher <bastian@eicher.net>
2020-12-26 23:39:27 +0100

    Fixed disposal of LocationsRedirect tests

--- src/UnitTests/TestWithRedirect.cs
+++ src/UnitTests/TestWithRedirect.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright Bastian Eicher et al.
 // Licensed under the GNU Lesser Public License
 
+using System;
 using NanoByte.Common.Storage;
 using Xunit;
 
@@ -10,7 +11,7 @@ namespace ZeroInstall
     /// Common base class for test fixtures that use a <see cref="LocationsRedirect"/>.
     /// </summary>
     [Collection("LocationsRedirect")]
-    public abstract class TestWithRedirect
+    public abstract class TestWithRedirect : IDisposable
     {
         private readonly LocationsRedirect _redirect = new("0install-unit-tests");
 
