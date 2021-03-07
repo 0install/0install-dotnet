505c497a4cb2e9ac1530f46d9c7032df5a3aad38
Bastian Eicher <bastian@eicher.net>
2021-03-07 20:02:21 +0100

    Tweaked ICommandHandler XML comments

--- src/Commands/ICommandHandler.cs
+++ src/Commands/ICommandHandler.cs
@@ -38,8 +38,8 @@ namespace ZeroInstall.Commands
         void CloseUI();
 
         /// <summary>
-        /// Shows the user the <see cref="Selections"/> made by the solver.
-        /// Returns immediately. Will be ignored by non-GUI interfaces.
+        /// Shows the <see cref="Selections"/> made by the solver to the user.
+        /// Returns immediately. May be ignored by some implementations.
         /// </summary>
         /// <param name="selections">The <see cref="Selections"/> as provided by the solver.</param>
         /// <param name="feedManager">The feed manager used to retrieve feeds for additional information about implementations.</param>
@@ -47,13 +47,14 @@ namespace ZeroInstall.Commands
 
         /// <summary>
         /// Allows the user to customize the interface preferences and rerun the solver if desired.
-        /// Returns once the user is satisfied with her choice. Will be ignored by non-GUI interfaces.
+        /// Returns once the user is finished.
         /// </summary>
         /// <param name="solveCallback">Called after interface preferences have been changed and the solver needs to be rerun.</param>
         void CustomizeSelections(Func<Selections> solveCallback);
 
         /// <summary>
         /// Displays application integration options to the user.
+        /// Returns once the user is finished.
         /// </summary>
         /// <param name="state">A View-Model for modifying the current desktop integration state.</param>
         /// <exception cref="OperationCanceledException">The user does not want any changes to be applied.</exception>
@@ -62,6 +63,7 @@ namespace ZeroInstall.Commands
 
         /// <summary>
         /// Displays a user interface for managing <see cref="IImplementationStore"/>s.
+        /// Returns once the user is finished.
         /// </summary>
         /// <param name="implementationStore">The <see cref="IImplementationStore"/> to manage.</param>
         /// <param name="feedCache">Information about implementations found in the <paramref name="implementationStore"/> are extracted from here.</param>
