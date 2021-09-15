f3ef5b54be48ca3758f7852b321a73178e515ec5
Bastian Eicher <bastian@eicher.net>
2021-09-15 17:26:41 +0200

    Introduced IconStores.Default

---
+++ src/Store/Icons/IconStores.cs
@@ -0,0 +1,28 @@
+﻿// Copyright Bastian Eicher et al.
+// Licensed under the GNU Lesser Public License
+
+using System;
+using System.IO;
+using NanoByte.Common.Storage;
+using NanoByte.Common.Tasks;
+using ZeroInstall.Store.Configuration;
+
+namespace ZeroInstall.Store.Icons
+{
+    /// <summary>
+    /// Provides <see cref="IIconStore"/> instances.
+    /// </summary>
+    public static class IconStores
+    {
+        /// <summary>
+        /// Creates an <see cref="IIconStore"/> instance that uses the default cache location.
+        /// </summary>
+        /// <exception cref="IOException">A problem occurred while creating a directory.</exception>
+        /// <exception cref="UnauthorizedAccessException">Creating a directory is not permitted.</exception>
+        public static IIconStore Default(ITaskHandler handler)
+            => new IconStore(
+                Locations.GetCacheDirPath("0install.net", false, "icons"),
+                Config.LoadSafe(),
+                handler);
+    }
+}
