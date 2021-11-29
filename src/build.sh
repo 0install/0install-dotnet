3e53c5dfe09b7da86763671d179863bc6a09ac4f
Bastian Eicher <bastian@eicher.net>
2021-11-29 21:35:00 +0100

    Improved build scripts

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 if command -v dotnet > /dev/null 2> /dev/null; then
     dotnet="dotnet"
 else
-    dotnet="../0install.sh run --version 5.0..!5.1 https://apps.0install.net/dotnet/core-sdk.xml"
+    dotnet="../0install.sh run --version 5.0.. https://apps.0install.net/dotnet/sdk.xml"
 fi
 
 # Build
