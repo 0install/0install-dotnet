4fef2968d80fb7fe0aa9940713a09b8fe89f9e47
Bastian Eicher <bastian@eicher.net>
2022-01-16 13:54:01 +0100

    Updated 0install bootstrap scripts

--- 0install.ps1
+++ 0install.ps1
@@ -1,18 +1,35 @@
 $ErrorActionPreference = "Stop"
 
-function download {
-    $downloadDir = "$env:LOCALAPPDATA\0install.net\bootstrapper"
-    if (!(Test-Path "$downloadDir\0install.exe")) {
-        mkdir -Force $downloadDir | Out-Null
+function Download-ZeroInstall {
+    $dir = "$env:LOCALAPPDATA\0install.net\bootstrapper"
+    $file = "$dir\0install.exe"
+    if (!(Test-Path $file)) {
+        mkdir -Force $dir | Out-Null
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]'Tls11,Tls12'
-        Invoke-WebRequest "https://get.0install.net/0install.exe" -OutFile "$downloadDir\0install.exe"
+        Invoke-WebRequest "https://get.0install.net/0install.exe" -OutFile $file
     }
-    return $downloadDir
+    return $file
 }
 
-$ErrorActionPreference = "SilentlyContinue"
-if (Get-Command 0install) {
-    0install @args | %{ "$_" }
+function Run-ZeroInstall {
+    if (Get-Command 0install -ErrorAction SilentlyContinue) {
+        0install @args
+    } else {
+        . $(Download-ZeroInstall) @args
+    }
+}
+
+if ($args.Count -eq 0) {
+    echo "This script runs 0install from your PATH or downloads it on-demand."
+    echo ""
+    echo "To run 0install commands without adding 0install to your PATH:"
+    echo ".\0install.ps1 COMMAND [OPTIONS]"
+    echo ""
+    echo "To deploy 0install to your user profile:"
+    echo ".\0install.ps1 self deploy"
+    echo ""
+    echo "To deploy 0install to your machine:"
+    echo ".\0install.ps1 self deploy --machine"
 } else {
-    . "$(download)\0install.exe" @args | %{ "$_" }
+    Run-ZeroInstall @args
 }
