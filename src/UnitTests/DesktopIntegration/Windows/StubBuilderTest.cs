50d4d20ad26507ff2943ad1c93be9e0fd561d785
Bastian Eicher <bastian@eicher.net>
2022-07-02 12:18:52 +0200

    Added unit tests to ensure deterministic generation of Windows stub EXEs

--- src/UnitTests/DesktopIntegration/Windows/StubBuilderTest.cs
+++ src/UnitTests/DesktopIntegration/Windows/StubBuilderTest.cs
@@ -2,6 +2,7 @@
 // Licensed under the GNU Lesser Public License
 
 using System.Runtime.Versioning;
+using System.Security.Cryptography;
 using NanoByte.Common.Native;
 using ZeroInstall.Store.Icons;
 
@@ -25,15 +26,27 @@ public class StubBuilderTest : TestWithRedirect
     {
         var target = new FeedTarget(FeedTest.Test1Uri, FeedTest.CreateTestFeed());
         target.Feed.EntryPoints[0].NeedsTerminal = true;
+
         var commandLine = _stubBuilder.GetRunCommandLine(target);
+
         commandLine.Should().HaveCount(1);
+        using var stream = File.OpenRead(commandLine[0]);
+        SHA1.Create().ComputeHash(stream)
+             // Ensure deterministic generation
+            .Should().Equal(0x0C, 0x01, 0xBF, 0xB8, 0xC3, 0x15, 0x32, 0x62, 0x1C, 0xF1, 0x91, 0x78, 0x4D, 0xDC, 0xC7, 0x11, 0x50, 0x04, 0x81, 0xBD);
     }
 
     [SkippableFact]
     public void TestGetRunCommandLineGui()
     {
         var target = new FeedTarget(FeedTest.Test1Uri, FeedTest.CreateTestFeed());
-        var commandLine = _stubBuilder.GetRunCommandLine(target, "");
+
+        var commandLine = _stubBuilder.GetRunCommandLine(target);
+
         commandLine.Should().HaveCount(1);
+        using var stream = File.OpenRead(commandLine[0]);
+        SHA1.Create().ComputeHash(stream)
+             // Ensure deterministic generation
+            .Should().Equal(0x0F, 0xCB, 0x80, 0xA7, 0x95, 0x2C, 0xBA, 0xA7, 0xC6, 0xB8, 0x5F, 0xFE, 0xE3, 0x3B, 0x6C, 0x1D, 0x9E, 0x6A, 0x43, 0x80);
     }
 }
