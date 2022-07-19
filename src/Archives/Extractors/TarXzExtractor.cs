86c96957b86482ee2795a7682e9e9f095a244a7f
Bastian Eicher <bastian@eicher.net>
2022-07-19 11:59:03 +0200

    Fixed "does not support seek" errors when extracting .tar.xz archives

--- src/Archives/Extractors/TarXzExtractor.cs
+++ src/Archives/Extractors/TarXzExtractor.cs
@@ -1,6 +1,7 @@
 // Copyright Bastian Eicher et al.
 // Licensed under the GNU Lesser Public License
 
+using NanoByte.Common.Streams;
 using SharpCompress.Compressors.Xz;
 using ZeroInstall.Store.FileSystem;
 
@@ -14,5 +15,7 @@ public partial class TarXzExtractor : TarExtractor
 {
     /// <inheritdoc/>
     public override void Extract(IBuilder builder, Stream stream, string? subDir = null)
-        => base.Extract(builder, new XZStream(stream), subDir);
+        => base.Extract(builder, new XZStream(
+            stream.WithSeekBuffer(bufferSize: 0) // Allow skipping past padding
+        ), subDir);
 }
