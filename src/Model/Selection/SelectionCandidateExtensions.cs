ac25567d1b7bc73e3a6a16538c668ccf80f6f68d
Bastian Eicher <bastian@eicher.net>
2022-10-10 19:34:47 +0200

    Added IEnumerable<SelectionCandidate>.GetSuitableVersions() extension method

---
+++ src/Model/Selection/SelectionCandidateExtensions.cs
@@ -0,0 +1,20 @@
+﻿// Copyright Bastian Eicher et al.
+// Licensed under the GNU Lesser Public License
+
+namespace ZeroInstall.Model.Selection;
+
+/// <summary>
+/// Provides extension methods for <see cref="SelectionCandidate"/>.
+/// </summary>
+public static class SelectionCandidateExtensions
+{
+    /// <summary>
+    /// Returns a deduplicated list of suitable version numbers, sorted from newest to oldest.
+    /// </summary>
+    public static IEnumerable<ImplementationVersion> GetSuitableVersions(this IEnumerable<SelectionCandidate> candidates)
+        => candidates.Where(x => x.IsSuitable)
+                     .Select(x => x.Version)
+                     .WhereNotNull()
+                     .Distinct()
+                     .OrderByDescending(x => x);
+}
