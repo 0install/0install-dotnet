9ff88f508c6cc6e5e58d1cbf7eaacb9bd1d0acf6
Bastian Eicher <bastian@eicher.net>
2022-12-09 21:31:31 +0100

    Use .NET SDK 7.0 and C# 11

--- src/test.sh
+++ src/test.sh
@@ -2,12 +2,7 @@
 set -e
 cd `dirname $0`
 
-# Find dotnet
-if command -v dotnet > /dev/null 2> /dev/null; then
-    dotnet="dotnet"
-else
-    dotnet="../0install.sh run --version 6.0.. https://apps.0install.net/dotnet/sdk.xml"
-fi
+dotnet="../0install.sh run --version 7.0.. https://apps.0install.net/dotnet/sdk.xml"
 
 # Unit tests (without .NET Framework)
-$dotnet test --no-build --configuration Release --framework net6.0 UnitTests/UnitTests.csproj
+$dotnet test --no-build --configuration Release --framework net7.0 UnitTests/UnitTests.csproj
