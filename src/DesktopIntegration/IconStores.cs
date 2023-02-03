b53b9ad41433ad156352152645e51697f49efc85
Bastian Eicher <bastian@eicher.net>
2023-02-03 10:42:50 +0100

    Added missing exception documentation to IconStores

--- src/DesktopIntegration/IconStores.cs
+++ src/DesktopIntegration/IconStores.cs
@@ -13,6 +13,8 @@ public static class IconStores
     /// <summary>
     /// Provides icon files for display in table- or tile-like GUIs. Files may be discarded later.
     /// </summary>
+    /// <exception cref="IOException">A problem occurred while creating a directory.</exception>
+    /// <exception cref="UnauthorizedAccessException">Creating a directory is not permitted.</exception>
     public static IIconStore Cache(Config config, ITaskHandler handler)
         => new IconStore(
             Locations.GetCacheDirPath("0install.net", false, "icons"),
@@ -22,6 +24,8 @@ public static class IconStores
     /// <summary>
     /// Provides icon files for use with desktop integration. Files will remain persisted.
     /// </summary>
+    /// <exception cref="IOException">A problem occurred while creating a directory.</exception>
+    /// <exception cref="UnauthorizedAccessException">Creating a directory is not permitted.</exception>
     public static IIconStore DesktopIntegration(Config config, ITaskHandler handler, bool machineWide)
         => new IconStore(
             IntegrationManager.GetDir(machineWide, "icons"),
