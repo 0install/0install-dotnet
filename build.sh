eebe50bdd81e0c41b6a5dddd00f5e374f67c454e
Bastian Eicher <bastian@eicher.net>
2023-02-23 09:32:39 +0100

    Fixed CI and build scripts

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 VERSION=${1:-1.0.0-pre}
 
 src/build.sh $VERSION
-if [[ $* != *--skip-test* ]]; then
+if [ "$2" != "--skip-test" ]; then
     src/test.sh
 fi
 ./0install.sh run https://apps.0install.net/0install/0template.xml 0install-dotnet.xml.template version=$VERSION
