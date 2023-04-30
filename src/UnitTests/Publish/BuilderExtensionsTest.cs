7f25467f12d3b58c10da393e34161c21c716283c
Bastian Eicher <bastian@eicher.net>
2023-04-30 17:17:05 +0200

    Publish: Improved unit tests for BuilderExtensions

--- src/UnitTests/Publish/BuilderExtensionsTest.cs
+++ src/UnitTests/Publish/BuilderExtensionsTest.cs
@@ -15,18 +15,34 @@ public class BuilderExtensionsTest
 
     private readonly SimpleCommandExecutor _executor = new();
     private readonly SilentTaskHandler _handler = new();
-    private readonly ManifestBuilder _dummyBuilder = new(ManifestFormat.Sha1New);
+    private readonly ManifestBuilder _builder = new(ManifestFormat.Sha1New);
 
     [Fact]
     public void AddArchive()
     {
         using var stream = typeof(RetrievalMethodExtensionsTest).GetEmbeddedStream("testArchive.zip");
         using var microServer = new MicroServer("archive.zip", stream);
+
         var archive = new Archive {Href = microServer.FileUri};
-        _dummyBuilder.Add(archive, _executor, _handler);
+        _builder.Add(archive, _executor, _handler);
 
         archive.MimeType.Should().Be(Archive.MimeTypeZip);
         archive.Size.Should().Be(stream.Length);
+        _builder.Manifest.Should().NotBeEmpty();
+    }
+
+    [Fact]
+    public void AddArchiveRelative()
+    {
+        using var tempDir = new TemporaryDirectory("0install-test");
+        typeof(RetrievalMethodExtensionsTest).CopyEmbeddedToFile("testArchive.zip", Path.Combine(tempDir, "archive.zip"));
+        _executor.Path = Path.Combine(tempDir, "feed.xml");
+
+        var archive = new Archive {Href = new("archive.zip", UriKind.Relative)};
+        _builder.Add(archive, _executor, _handler);
+
+        archive.MimeType.Should().Be(Archive.MimeTypeZip);
+        _builder.Manifest.Should().NotBeEmpty();
     }
 
     [Fact]
@@ -34,10 +50,12 @@ public class BuilderExtensionsTest
     {
         using var stream = SingleFileData.ToStream();
         using var microServer = new MicroServer(SingleFileName, stream);
+
         var file = new SingleFile {Href = microServer.FileUri, Destination = SingleFileName};
-        _dummyBuilder.Add(file, _executor, _handler);
+        _builder.Add(file, _executor, _handler);
 
         file.Size.Should().Be(stream.Length);
+        _builder.Manifest.Should().NotBeEmpty();
     }
 
     [Fact]
@@ -45,11 +63,13 @@ public class BuilderExtensionsTest
     {
         using var stream = typeof(RetrievalMethodExtensionsTest).GetEmbeddedStream("testArchive.zip");
         using var microServer = new MicroServer("archive.zip", stream);
+
         var archive = new Archive {Href = microServer.FileUri};
         var recipe = new Recipe {Steps = {archive}};
-        _dummyBuilder.Add(recipe, _executor, _handler);
+        _builder.Add(recipe, _executor, _handler);
 
         archive.MimeType.Should().Be(Archive.MimeTypeZip);
         archive.Size.Should().Be(stream.Length);
+        _builder.Manifest.Should().NotBeEmpty();
     }
 }
