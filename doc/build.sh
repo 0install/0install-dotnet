5d214477b020c60092da3cab80015f7ec2889eef
Bastian Eicher <bastian@eicher.net>
2023-04-30 20:11:29 +0200

    Docs: Updated DocFX

--- doc/build.sh
+++ doc/build.sh
@@ -2,4 +2,4 @@
 set -e
 cd `dirname $0`
 
-../0install.sh run --version=2.65.1 https://apps.0install.net/dotnet/docfx.xml --loglevel=warning --warningsAsErrors docfx.json
+../0install.sh run https://apps.0install.net/dotnet/docfx.xml --logLevel=warning --warningsAsErrors docfx.json
