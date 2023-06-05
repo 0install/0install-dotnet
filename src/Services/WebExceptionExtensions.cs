4397427be327f75619ac3ddc27bca6af2857f42f
Bastian Eicher <bastian@eicher.net>
2023-06-05 03:54:12 +0200

    Improved .ShouldTryMirror() check

--- src/Services/WebExceptionExtensions.cs
+++ src/Services/WebExceptionExtensions.cs
@@ -12,6 +12,13 @@ public static class WebExceptionExtensions
     /// Determines a download from a specific <paramref name="uri"/> that failed with an <paramref name="exception"/> is should be retried using a mirror location.
     /// </summary>
     public static bool ShouldTryMirror(this WebException exception, Uri uri)
-        => exception.Response is not HttpWebResponse {StatusCode: HttpStatusCode.Unauthorized or HttpStatusCode.Forbidden}
-        && uri is {Scheme: "http" or "https", IsLoopback: false};
+        => uri is {Scheme: "http" or "https", HostNameType: UriHostNameType.Dns, IsLoopback: false}
+        && exception is not {
+#if NET
+               InnerException: HttpRequestException
+#else
+               Response: HttpWebResponse
+#endif
+               {StatusCode: HttpStatusCode.Unauthorized or HttpStatusCode.Forbidden}
+           };
 }
