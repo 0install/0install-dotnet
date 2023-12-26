915058daec0af2193e58e71b68f4966ba60eb769
Bastian Eicher <bastian@eicher.net>
2023-12-27 00:38:10 +0100

    Use pre-installed .NET SDK for building on Linux if available

--- src/build.sh
+++ src/build.sh
@@ -3,7 +3,11 @@ set -e
 cd `dirname $0`
 
 # Find dotnet
-dotnet="../0install.sh run --version 7.0.. https://apps.0install.net/dotnet/sdk.xml"
+if command -v dotnet > /dev/null 2> /dev/null; then
+    dotnet="dotnet"
+else
+    dotnet="../0install.sh run --version 7.0.. https://apps.0install.net/dotnet/sdk.xml"
+fi
 
 # Build
 $dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True}
