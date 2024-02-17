7d2f791c2407095e1125667ae8d9e26a3c989908
Bastian Eicher <bastian@eicher.net>
2024-02-17 16:10:14 +0100

    Workaround for outdated .NET SDK on Linux CI

--- doc/build.sh
+++ doc/build.sh
@@ -3,11 +3,11 @@ set -e
 cd `dirname $0`
 
 # Find dotnet
-if command -v dotnet > /dev/null 2> /dev/null; then
-    dotnet="dotnet"
-else
+#if command -v dotnet > /dev/null 2> /dev/null; then
+#    dotnet="dotnet"
+#else
     dotnet="../0install.sh run --version 8.0.. https://apps.0install.net/dotnet/sdk.xml"
-fi
+#fi
 
 # Build docs
 $dotnet tool restore
--- src/build.sh
+++ src/build.sh
@@ -3,11 +3,11 @@ set -e
 cd `dirname $0`
 
 # Find dotnet
-if command -v dotnet > /dev/null 2> /dev/null; then
-    dotnet="dotnet"
-else
+#if command -v dotnet > /dev/null 2> /dev/null; then
+#    dotnet="dotnet"
+#else
     dotnet="../0install.sh run --version 8.0.. https://apps.0install.net/dotnet/sdk.xml"
-fi
+#fi
 
 # Build
 $dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True}
