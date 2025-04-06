94e5a1dabb9d7839edb45581460ada5d0e9bf80d
Bastian Eicher <bastian@eicher.net>
2025-04-06 16:25:58 +0200

    Tweaked build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -7,16 +7,15 @@ function Run-DotNet {
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
-if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True /terminalLogger:off" }
-
-echo "Build minimal binaries"
-Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Minimal /p:Version=$Version $ci
-
 echo "Build binaries"
+if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
 Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version $ci
 
 echo "Prepare binaries for publishing"
 Run-DotNet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences=False /p:Configuration=Release /p:TargetFramework=net8.0 /p:Version=$Version Commands
 Remove-Item ..\artifacts\Release\net8.0\publish\* -Include *.xml,*.pdb
+
+echo "Build minimal binaries"
+Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Minimal /p:Version=$Version
 
 popd
--- src/build.sh
+++ src/build.sh
@@ -9,9 +9,17 @@ cd `dirname $0`
     dotnet="../0install.sh run --version 9.0.. https://apps.0install.net/dotnet/sdk.xml"
 #fi
 
+# Avoid terminal rendering issues in CI
+if [ -n "$CI" ]; then
+    export MSBUILDTERMINALLOGGER="off"
+fi
+
 echo "Build binaries"
-$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True -terminalLogger:off}
+$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True}
 
 echo "Prepare binaries for publishing"
 $dotnet msbuild -v:Quiet -t:Publish -p:NoBuild=True -p:BuildProjectReferences=False -p:Configuration=Release -p:TargetFramework=net8.0 -p:Version=${1:-1.0.0-pre} Commands
 find ../artifacts/Release/net8.0/publish -name "{*.xml,*.pdb}" -type f -delete
+
+echo "Build minimal binaries"
+$dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Minimal -p:Version=${1:-1.0.0-pre}
