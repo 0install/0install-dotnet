56c6e24e32a77ea8c9bccd5ea1bbc91d7b0b360e
Bastian Eicher <bastian@eicher.net>
2025-04-06 19:59:58 +0200

    Added Bootstrapper (wip)

--- src/build.ps1
+++ src/build.ps1
@@ -17,4 +17,7 @@ Run-DotNet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences
 echo "Build minimal binaries"
 Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Minimal /p:Version=$Version
 
+echo "Link bootstrapper"
+Run-DotNet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences=False /p:Configuration=Minimal /p:TargetFramework=net9.0 /p:RuntimeIdentifier=win-x64 /p:Version=$Version Bootstrap
+
 popd
