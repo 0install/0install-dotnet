be5c3cf20efc4df0aaeb420bc602cd5e2b66e700
Bastian Eicher <bastian@eicher.net>
2025-06-04 19:00:22 +0200

    Update SharpCompress

--- src/Archives/Extractors/TarLzmaExtractor.cs
+++ src/Archives/Extractors/TarLzmaExtractor.cs
@@ -3,6 +3,7 @@
 
 #if !MINIMAL
 using NanoByte.Common.Streams;
+using SharpCompress.Common;
 using SharpCompress.Compressors.LZMA;
 using ZeroInstall.Store.FileSystem;
 
@@ -17,6 +18,18 @@ public class TarLzmaExtractor(ITaskHandler handler) : TarExtractor(handler)
 {
     /// <inheritdoc/>
     public override void Extract(IBuilder builder, Stream stream, string? subDir = null)
-        => base.Extract(builder, new LzmaStream(stream.Read(13), stream), subDir);
+    {
+        try
+        {
+            base.Extract(builder, new LzmaStream(stream.Read(13), stream), subDir);
+        }
+        #region Error handling
+        catch (ExtractionException ex)
+        {
+            // Wrap exception since only certain exception types are allowed
+            throw new IOException(Resources.ArchiveInvalid, ex);
+        }
+        #endregion
+    }
 }
 #endif
--- src/Archives/Extractors/TarXzExtractor.cs
+++ src/Archives/Extractors/TarXzExtractor.cs
@@ -3,6 +3,7 @@
 
 #if !MINIMAL
 using NanoByte.Common.Streams;
+using SharpCompress.Common;
 using SharpCompress.Compressors.Xz;
 using ZeroInstall.Store.FileSystem;
 
@@ -17,8 +18,20 @@ public class TarXzExtractor(ITaskHandler handler) : TarExtractor(handler)
 {
     /// <inheritdoc/>
     public override void Extract(IBuilder builder, Stream stream, string? subDir = null)
-        => base.Extract(builder, new XZStream(
-            stream.WithSeekBuffer(bufferSize: 0) // Allow skipping past padding
-        ), subDir);
+    {
+        try
+        {
+            base.Extract(builder, new XZStream(
+                stream.WithSeekBuffer(bufferSize: 0) // Allow skipping past padding
+            ), subDir);
+        }
+        #region Error handling
+        catch (ExtractionException ex)
+        {
+            // Wrap exception since only certain exception types are allowed
+            throw new IOException(Resources.ArchiveInvalid, ex);
+        }
+        #endregion
+    }
 }
 #endif
