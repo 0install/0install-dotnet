316b602298070b11730bda2d73b4e7354b7bbb5b
Bastian Eicher <bastian@eicher.net>
2025-08-14 19:41:19 +0200

    Tweaked build script

--- build.ps1
+++ build.ps1
@@ -5,6 +5,6 @@ pushd $PSScriptRoot
 src\build.ps1 $Version
 if (-Not $SkipTest) { src\test.ps1 }
 doc\build.ps1
-.\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml 0install-dotnet.xml.template version=$Version `
+.\0install.ps1 run --batch https://apps.0install.net/0install/0template.xml 0install-dotnet.xml.template version=$Version
 
 popd
