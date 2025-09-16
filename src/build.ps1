fca53587354f04cf6091ff61661315abf58a132c
Bastian Eicher <bastian@eicher.net>
2025-09-16 21:33:12 +0200

    Switched to .slnx format

--- src/build.ps1
+++ src/build.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 pushd $PSScriptRoot
 
 function Run-DotNet {
-    ..\0install.ps1 run --batch --version 9.0.. https://apps.0install.net/dotnet/sdk.xml @args
+    ..\0install.ps1 run --batch --version 9.0.200.. https://apps.0install.net/dotnet/sdk.xml @args
     if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
 }
 
