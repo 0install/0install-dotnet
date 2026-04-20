1dbec793ce60abc70b08ebc1bbf7f5e0600b8d1f
Bastian Eicher <bastian@eicher.net>
2026-04-20 11:59:13 +0200

    Added .NET 10.0 build target

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,7 @@ cd `dirname $0`
 #if command -v dotnet > /dev/null 2> /dev/null; then
 #    dotnet="dotnet"
 #else
-    dotnet="../0install.sh run --version 9.0.200.. https://apps.0install.net/dotnet/sdk.xml"
+    dotnet="../0install.sh run --version 10.0.. https://apps.0install.net/dotnet/sdk.xml"
 #fi
 
 # Avoid terminal rendering issues in CI
@@ -18,8 +18,8 @@ echo "Build binaries"
 $dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Release -p:Version=${1:-1.0.0-pre} ${CI+-p:ContinuousIntegrationBuild=True}
 
 echo "Prepare binaries for publishing"
-$dotnet msbuild -v:Quiet -t:Publish -p:NoBuild=True -p:BuildProjectReferences=False -p:Configuration=Release -p:TargetFramework=net8.0 -p:Version=${1:-1.0.0-pre} Commands
-find ../artifacts/Release/net8.0/publish -name "{*.xml,*.pdb}" -type f -delete
+$dotnet msbuild -v:Quiet -t:Publish -p:NoBuild=True -p:BuildProjectReferences=False -p:Configuration=Release -p:TargetFramework=net10.0 -p:Version=${1:-1.0.0-pre} Commands
+find ../artifacts/Release/net10.0/publish -name "{*.xml,*.pdb}" -type f -delete
 
 echo "Build minimal binaries"
 $dotnet msbuild -v:Quiet -t:Restore -t:Build -p:Configuration=Minimal -p:Version=${1:-1.0.0-pre}
