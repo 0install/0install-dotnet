ecb361ecc6ddcf420c27055153638a55cfd4790a
Bastian Eicher <bastian@eicher.net>
2026-06-26 12:00:23 +0200

    Tweaked build scripts

--- src/build.ps1
+++ src/build.ps1
@@ -9,12 +9,12 @@ function Run-DotNet {
 
 echo "Build binaries"
 if ($env:CI) { $ci = "/p:ContinuousIntegrationBuild=True" }
-Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Release /p:Version=$Version $ci
+Run-DotNet msbuild /v:Quiet /Restore /t:Build /p:Configuration=Release /p:Version=$Version $ci
 
 echo "Prepare binaries for publishing"
 Run-DotNet msbuild /v:Quiet /t:Publish /p:NoBuild=True /p:BuildProjectReferences=False /p:Configuration=Release /p:TargetFramework=net8.0 /p:Version=$Version Commands
 
 echo "Build minimal binaries"
-Run-DotNet msbuild /v:Quiet /t:Restore /t:Build /p:Configuration=Minimal /p:Version=$Version
+Run-DotNet msbuild /v:Quiet /Restore /t:Build /p:Configuration=Minimal /p:Version=$Version
 
 popd
