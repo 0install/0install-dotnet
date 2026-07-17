3975eb15bdf44483f7d2522055967f5e8a7330ac
Bastian Eicher <bastian@eicher.net>
2026-07-17 12:33:05 +0200

    Made CategoryIntegrationManager.All|StandardCategories immutable

--- src/DesktopIntegration/CategoryIntegrationManager.cs
+++ src/DesktopIntegration/CategoryIntegrationManager.cs
@@ -19,10 +19,10 @@ public class CategoryIntegrationManager(Config config, ITaskHandler handler, boo
 {
     #region Constants
     /// <summary>A list of all known <see cref="AccessPoint"/> categories.</summary>
-    public static readonly string[] AllCategories = [CapabilityRegistration.TagName, MenuEntry.TagName, DesktopIcon.TagName, SendTo.TagName, AppAlias.TagName, AutoStart.TagName, DefaultAccessPoint.TagName];
+    public static IReadOnlyList<string> AllCategories { get; } = [CapabilityRegistration.TagName, MenuEntry.TagName, DesktopIcon.TagName, SendTo.TagName, AppAlias.TagName, AutoStart.TagName, DefaultAccessPoint.TagName];
 
     /// <summary>A list of recommended standard <see cref="AccessPoint"/> categories.</summary>
-    public static readonly string[] StandardCategories = [CapabilityRegistration.TagName, MenuEntry.TagName, SendTo.TagName, AppAlias.TagName];
+    public static IReadOnlyList<string> StandardCategories { get; } = [CapabilityRegistration.TagName, MenuEntry.TagName, SendTo.TagName, AppAlias.TagName];
     #endregion
 
     //--------------------//
